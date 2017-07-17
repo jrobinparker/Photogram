f6f6d7f9360e22de3bef136fac12b125f866f52c
Jeremy Parker <jeremyparker@Jeremys-MacBook-Air.local>
2017-07-17 16:43:06 -0700

    Updated posts controller with create action.

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ gem 'puma', '~> 3.7'
 gem 'sass-rails', '~> 5.0'
 # paperclip for uploading photos
 gem 'paperclip', '~> 5.1'
+gem 'simple_form', '~> 3.5'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,17 @@
 class Post < ActiveRecord::Base
   validates :image, presence: true
 
-  has_attached_file :image, styles { :medium => "640x" }
+  has_attached_file :image, styles: { :medium => "640x" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+  def create
+    @post = Post.create(post_params)
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:image, :caption)
+  end
 end
