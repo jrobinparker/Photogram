de0f6e30f38ae88656f9629167bea316206db568
Jeremy Parker <jeremyparker@Jeremys-MacBook-Air.local>
2017-07-17 21:13:10 -0700

    Added flashes for alerts and updated controller to find specific post by the id parameter.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
   def index
     @posts = Post.all
   end
@@ -9,8 +9,13 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.create(post_params)
-    redirect_to posts_path
+    if @post = Post.create(post_params)
+      flash[:success] = "Your post has been created!"
+      redirect_to posts_path
+    else
+      flash.now[:alert] = "Your new post couldn't be created! Please check the form."
+      render :new
+    end
   end
 
   def show
@@ -23,8 +28,13 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
-    @post.update(post_params)
-    redirect_to(post_path(@post))
+    if @post.update(post_params)
+      flash[:success] = "Post updated."
+      redirect_to(post_path(@post))
+    else
+      flash.now[:alert] = "Update failed. Please check the form."
+      render :edit
+    end
   end
 
   def destroy
@@ -37,6 +47,10 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:image, :caption)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def alert_for(flash_type)
+    { success: 'alert-success',
+      error: 'alert-danger',
+      alert: 'alert-warning',
+      notie: 'alert-info'
+    }[flash_type.to_sym] || flash_type.to_s
+  end
+
 end
